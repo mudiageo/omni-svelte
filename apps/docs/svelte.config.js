19f1b038902d64f15ec724177b30574c847b961c
Mudiaga Arharhire <mudiageo@gmail.com>
2026-06-23 23:12:37 +0000

    chore(docs): init shadcn-svelte

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,12 +1,20 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsx } from 'mdsx';
+import { mdsxConfig } from './mdsx.config.js';
 
-/** @type {import('./src').SvelteConfig} */
+/** @type {import('omni-svelte').SvelteConfig} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	preprocess: [mdsx(mdsxConfig), vitePreprocess()],
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$content: './content',
+			$velite: './.velite'
+		}
+	},
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
