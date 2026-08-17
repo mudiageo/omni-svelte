ac858fb8f07069aa0ad559f9b0971663abb02f82
Mudiaga Arharhire <mudiageo@gmail.com>
2026-08-17 20:06:23 +0000

    fix(docs): move sveltekit config to vite.config.ts and remove svelte.config.js

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,10 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { velitePlugin } from './velite.config.ts';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsx } from 'mdsx';
+import { mdsxConfig } from './mdsx.config.js';
+
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 export const veliteDirPath = path.join(__dirname, '.velite');
 export const contentDirPath = path.join(__dirname, '../../content');
@@ -12,7 +16,30 @@ export const packagesDirPath = path.join(__dirname, '../../packages');
 // export const ogDirPath = path.join(__dirname, "src/routes/og");
 
 export default defineConfig({
-	plugins: [tailwindcss(), velitePlugin(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		velitePlugin(),
+		sveltekit({
+			adapter: adapter(),
+			alias: {
+				$content: '../../content',
+				$velite: './.velite'
+			},
+			prerender: {
+				handleMissingId: (details) => {
+					if (details.id === '#') return;
+					console.warn(details.message);
+				},
+				handleHttpError: (details) => {
+					// TODO: remove once all referenced pages are added
+					console.warn(details.message);
+				},
+				handleUnseenRoutes: 'ignore'
+			},
+			preprocess: [vitePreprocess(), mdsx(mdsxConfig)],
+			extensions: ['.svelte', '.md']
+		})
+	],
 	server: {
 		fs: {
 			allow: [veliteDirPath, contentDirPath, packagesDirPath]
