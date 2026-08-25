98386885664c24a07bd75f36c38f1bd7a51667eb
omni-svelte maintainer <maintainer@omni-svelte.dev>
2026-08-25 19:03:30 +0000

    chore: remove test files accidentally committed

--- test-aggregate-assign.js
+++
@@ -1,6 +0,0 @@
-try {
-    throw new AggregateError([new Error("inner")], "Agg");
-} catch (err) {
-    err.stack = "new stack";
-    console.log("Assigned successfully:", err.stack);
-}
--- test-aggregate.js
+++
@@ -1,6 +0,0 @@
-try {
-    throw new AggregateError([new Error("inner")], "Agg");
-} catch (err) {
-    const stackDesc = Object.getOwnPropertyDescriptor(err, 'stack');
-    console.log("Descriptor:", stackDesc);
-}
